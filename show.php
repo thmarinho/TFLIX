0f8bd5d8c5e9174571479b07b4505d4af73fb6fd
thmarinho <theo.marinho@epitech.eu>
2021-06-16 20:50:13 +0200

    [fix] fix css

--- show.php
+++ show.php
@@ -12,7 +12,7 @@
   </head>
 	<body>
     <?php include('./components/header.php') ?>
-		<a class='fab-top-right' href='/shows.php'>
+		<a class='fab-top-left' href='/shows.php'>
       <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M4 12L20 12M4 12L10 6M4 12L10 18" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
